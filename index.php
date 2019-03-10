f1bcbf7806f04d9bf2df9c8d59f1a739aef8ed1d
Sam Bonnekamp <Samuel@Bonnekamp.net>
2019-03-10 18:58:43 +0800

    finished up login sys entirely, also commented (youre welcome)

--- index.php
+++ index.php
@@ -4,52 +4,73 @@
 $teacher = False;
 $mysqli = new mysqli("localhost", "root", "", "userbase");
 $fiesta = "";
-	// if (isset($_COOKIE["user"]) || isset($_COOKIE["secret"])) {
-	// 	$usernameQuery = "SELECT special FROM userData WHERE username ='" . $_COOKIE["user"] . "'";
-	// 	$usernameResult = $mysqli->query($usernameQuery);
-	// 	$usernameRow = implode($usernameResult->fetch_assoc());
-	// 		if ($usernameRow === $_COOKIE["secret"]) {
-	// 			header("Location:/dashboard/index.php");
-	// 		} else {
-	// 		}
-	// }
+$message = "";
+#just a bunch of stuff to initialise the system
+
+if (isset($_COOKIE["ident"])) {
+	if  (isset($_COOKIE["secure"])){ #checks if the cookie belongs to admin account as regular accounts dont have the secure cookie (line 62)
+		$ident = $_COOKIE["ident"];
+		$glasses = "SELECT id FROM userData WHERE username = '" . $ident . "'"; #takes the id based on the ident cookie
+		$glassesQuery = $mysqli->query($glasses) or die($mysqli->error);
+		$glassRow = implode($glassesQuery->fetch_assoc());
+		$check = md5($glassRow . $_COOKIE["ident"]); #hashes it to re create how the original cookie was written (secure cookie hash was created with id + user)
+		if ($check == $_COOKIE["secure"]){
+			header("Location: /dashboard/index.php"); #here if it matches the cookie
+		}
+	}else {
+		header("Location: /dashboard/index.php"); #goes to this condition if cookie is set and is not teacher account (can be faked but, might aswell just use the form lmao)
+	}
+ }
+#cookie checking script
+
 if ($mysqli->connect_error) {
 	die("Connection failed: 0x636f6e6572726f72 " . $mysqli->connect_error);
 	echo("This is an error, please report it via email to samuel@bonnekamp.net");
 } 
+#make sure the mysql server is still alive
+
 if(isset($_POST["username"])){
-	if ($_POST["username"] == "18bonnekampsb2"){
+	if ($_POST["username"] == "18bonnekampsb2"){ #hard coded the admin accounts (dont sue me). just put in aniother select and youll be good. 
 		$teacher = True;
-		$fiesta = $_POST["username"];
+		$fiesta = $_POST["username"]; #adds the second input field (check the actual html im too lazy to quote)
+
 		if (isset($_POST["password"])){
-			$passCheck = "SELECT password FROM userData WHERE username = '" . $_POST["username"] . "'";
+			$passCheck = "SELECT password FROM userData WHERE username = '" . $_POST["username"] . "'"; #takes password
 			$passCheckQuery = $mysqli->query($passCheck) or die($mysqli->error);
 			$passRow = implode($passCheckQuery->fetch_assoc());
-			if ($passRow == $_POST["password"]){
+
+			if ($passRow == $_POST["password"]){ #checks the password
 				$idSel = "SELECT id FROM userData WHERE username = '" . $_POST["username"] . "'";
 				$idSelQuery = $mysqli->query($idSel) or die($mysqli->error);
 				$idRow = implode($idSelQuery->fetch_assoc());
 				$check = md5($idRow . $_POST["username"]);
-				setcookie("ident", $_POST["username"], time() + (86400 * 30), "/");
+				setcookie("ident", $_POST["username"], time() + (86400 * 30), "/"); #if its correct, generates secure hash with id and username (schould be secure enough)
 				setcookie("secure", $check, time() + (86400 * 30), "/");
 				header("Location: /dashboard/index.php");
+			}else {
+				$GLOBALS["message"] = "Username and Password do not match."; #take a wild guess
 			}
 		}
-	}else {
+
+		}else {
 		$username = $_POST["username"];
 		$GLOBALS["message"] = "";
 
 		$check = "SELECT username FROM userData WHERE username ='" . $username . "'";
 		$checkQuery = $mysqli->query($check) or die($mysqli->error);
-		if (!empty($checkQuery)){
+		$checkRow = implode($checkQuery->fetch_assoc());
+
+		if ($username == $checkRow){ #checks if the inputted username actually exists, and if so, headers them to dash
 			header("Location: /dashboard/index.php");
-			setcookie("ident", $_POST["username"], time() + (86400 * 30), "/");
+			setcookie("ident", $_POST["username"], time() + (86400 * 30), "/"); #sets cookie based solely on username (dont need cookie security when its just a username)
+		}else {
+			$GLOBALS["message"] = "User doesn't exist";
 		}
 	}
-	
+	#this section is for the plebians without admin accounts
 	
 }
-
+#main body of the script, wow I actually commented this!
 ?>
 <!DOCTYPE html>
 <html>
@@ -65,8 +86,6 @@ if(isset($_POST["username"])){
 
 <body>
 <?php
-error_reporting(0);
-ini_set('display_errors', 0);
 
 ?>
    
